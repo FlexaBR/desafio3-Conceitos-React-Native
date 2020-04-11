3784de7ab0a4217dc9f13b682a8fb771e54f1392
FlexaBR <r.gutheil@terra.com.br>
2020-04-11 20:15:32 -0300

    alterando a baseURL para correção desafio

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:3333",
+  baseURL: "http://localhost:3333",
   // baseURL: 'http://192.168.0.15:3333',
-  baseURL: 'http://10.1.1.100:3333',
+  // baseURL: 'http://10.1.1.100:3333',
 });
 
 export default api;
